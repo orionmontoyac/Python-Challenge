de1768d6153ce22e086b83b423642cffc856d3f6
Orion Montoya C <32209943+orionmontoyac@users.noreply.github.com>
2017-09-28 14:09:40 -0500

    Terminado
    Se agrego unas funcionalidades extras al filtro.

--- level_3.py
+++ level_3.py
@@ -13,8 +13,7 @@ string = "".join(re.findall("[a-zA-Z]",contenido))
 
 for i in range(3,len(string)-7):
     if 97 <= ord(string[i]) <= 122:
-        if (65 <= ord(string[i+1]) <= 90) and (65 <= ord(string[i+2]) <= 90) and (65 <= ord(string[i+3]) <= 90): 
-            if (65 <= ord(string[i-1]) <= 90) and (65 <= ord(string[i-2]) <= 90) and (65 <= ord(string[i-3]) <= 90):          
-                print(string[i-3] + string[i-2] + string[i-1] +" "+ string[i] +" "+string[i+1] + string[i+2] + string[i+3])
-                
+        if (65 <= ord(string[i+1]) <= 90) and (65 <= ord(string[i+2]) <= 90) and (65 <= ord(string[i+3]) <= 90) and (string[i+4] == string[i+4].lower()): 
+            if (65 <= ord(string[i-1]) <= 90) and (65 <= ord(string[i-2]) <= 90) and (65 <= ord(string[i-3]) <= 90) and (string[i-4] == string[i-4].lower()):          
+                print(string[i-4] + string[i-3] + string[i-2] + string[i-1] +" "+ string[i] +" "+string[i+1] + string[i+2] + string[i+3] + string[i+4])                
     
