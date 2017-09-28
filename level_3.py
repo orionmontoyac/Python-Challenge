8432413d9f51730718499e941e0b4a707724543a
Orion-PC <bigbang.bigcrunsh@gmail.com>
2017-09-28 09:24:45 -0500

    Patron econtrado
    Encontrado las letras con 3 mayusculas a   amnbos lados

---
+++ level_3.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 28 08:51:19 2017
+
+@author: Orion
+"""
+import re
+mess2 = open('mess2.txt', 'r')
+
+contenido = mess2.read()
+
+string = "".join(re.findall("[a-zA-Z]",contenido))
+
+for i in range(3,len(string)-7):
+    if 97 <= ord(string[i]) <= 122:
+        if (65 <= ord(string[i+1]) <= 90) and (65 <= ord(string[i+2]) <= 90) and (65 <= ord(string[i+3]) <= 90): 
+            if (65 <= ord(string[i-1]) <= 90) and (65 <= ord(string[i-2]) <= 90) and (65 <= ord(string[i-3]) <= 90):          
+                print(string[i-3] + string[i-2] + string[i-1] +" "+ string[i] +" "+string[i+1] + string[i+2] + string[i+3])
+                
+    
