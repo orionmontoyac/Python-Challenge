7cea8d690574a401afcb60a716c3a22cd9f4dc04
orion <bigbang.bigcrunsh@gmail.com>
2019-08-12 11:49:56 -0500

    Se agrega el nivel 1

---
+++ level_1.py
@@ -0,0 +1,6 @@
+
+string = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. "
+in_string =  "yzabcdefghijklmnopqrstuvwx"
+out_string = "abcdefghijklmnopqrstuvwxyz"
+trantab = str.maketrans(in_string, out_string)
+print (string.translate(trantab))
